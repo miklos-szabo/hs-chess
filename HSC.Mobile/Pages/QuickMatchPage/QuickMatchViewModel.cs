b7bd6911566ae798fa0f8494c5854c867e575a0c
Szabó Miklós <szabo.miklos@ma-coding.com>
2022-11-22 18:51:27 +0100

    xaml stuff

--- HSC.Mobile/Pages/QuickMatchPage/QuickMatchViewModel.cs
+++ HSC.Mobile/Pages/QuickMatchPage/QuickMatchViewModel.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Keycloak.Net;
+using Keycloak.Net.Models.ClientInitialAccess;
 
 namespace HSC.Mobile.Pages.QuickMatchPage
 {
     public class QuickMatchViewModel: BaseViewModel
     {
+        public ICommand SearchCommand { get; set; }
+        public ICommand createCustomCommand { get; set; }
+
+        public QuickMatchViewModel()
+        {
+            SearchCommand = new Command(async () => await SearchForMatch());
+        }
+
+        public async Task SearchForMatch()
+        {
+            var x = new KeycloakClient("http://hsckeycloak10.c5dzcec2bngmbcds.eastus.azurecontainer.io:8080", "user2", "user2");
+            var ahh = await x.CreateInitialAccessTokenAsync("chess", new ClientInitialAccessCreatePresentation());
+            Console.WriteLine(ahh.Token);
+        }
     }
 }
