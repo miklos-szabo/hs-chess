f9ffc674f46112f7757987950a2f699de314a3a7
Szabó Miklós <szabo.miklos@ma-coding.com>
2022-12-04 16:31:07 +0100

    cashier

--- HSC.Mobile/Pages/CashierPage/CashierViewModel.cs
+++ HSC.Mobile/Pages/CashierPage/CashierViewModel.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using HSCApi;
 
 namespace HSC.Mobile.Pages.CashierPage
 {
     public class CashierViewModel: BaseViewModel
     {
+        private readonly AccountService _accountService;
+        private decimal _balance;
+
+        public ICommand Add100Command { get; set; }
+
+        public decimal Balance
+        {
+            get => _balance;
+            set => SetField(ref _balance, value);
+        }
+
+        public CashierViewModel(AccountService accountService)
+        {
+            _accountService = accountService;
+
+            Add100Command = new Command(async () => await Add100());
+
+            Task.Run(async () =>
+            {
+                var data = await _accountService.GetUserMenuDataAsync();
+                Balance = data.Balance;
+            });
+        }
+
+        public async Task Add100()
+        {
+            await _accountService.AddMoneyAsync(100);
+            var data = await _accountService.GetUserMenuDataAsync();
+            Balance = data.Balance;
+        }
+
+
     }
 }
